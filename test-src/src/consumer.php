b8131ca20a89993bf35e5b3b67756c92e7bd43c2
Maxim Kapkaev <makm@km.ru>
2020-02-27 16:32:30 +0300

    refactoring

--- test-src/src/consumer.php
+++ test-src/src/consumer.php
@@ -6,7 +6,8 @@ if (empty($argv[1])) {
     die('set priority queue name');
 }
 
-define('TEST_PROCESS_SLEEP_TIME', 2);
+define('TEST_PROCESS_SLEEP_TIME', 2); // sleep for test
+define('MAX_BATCH_COUNT', 5); // count messages per batch (in group)
 $queueName = $argv[1];
-$taskConsumer = $application->createTaskConsumer($queueName, 5);
+$taskConsumer = $application->createTaskConsumer($queueName, MAX_BATCH_COUNT);
 $taskConsumer->work();
